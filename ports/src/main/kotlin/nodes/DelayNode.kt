39574724a5cd71dd56be707b214655c544c06dc8
ms6123 <ms6123@imperial.ac.uk>
2026-01-18 16:09:37 +0000

    Improvement: Report occupants in DelayNode.

--- ports/src/main/kotlin/nodes/DelayNode.kt
+++ ports/src/main/kotlin/nodes/DelayNode.kt
@@ -1,6 +1,7 @@
 package com.group7.nodes
 
 import com.group7.InputChannel
+import com.group7.Metrics
 import com.group7.Node
 import com.group7.OutputChannel
 import com.group7.generators.DelayProvider
@@ -12,7 +13,17 @@ class DelayNode<T>(
     delayProvider: DelayProvider,
 ) : Node(label, listOf(destination)) {
 
+    private var occupants = 0
+
     init {
-        source.onReceive { scheduleDelayed(delayProvider.nextDelay()) { destination.send(it) } }
+        source.onReceive {
+            occupants++
+            scheduleDelayed(delayProvider.nextDelay()) {
+                destination.send(it)
+                occupants--
+            }
+        }
     }
+
+    override fun reportMetrics() = Metrics(occupants = occupants)
 }
