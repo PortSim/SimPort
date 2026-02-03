1b7de0032b208c84b56691d0017e3a1cb3551120
ms6123 <ms6123@imperial.ac.uk>
2026-02-03 16:36:21 +0000

    Improvement: Initialize ForkPolicy with simulator.

--- ports/src/main/kotlin/nodes/ForkNode.kt
+++ ports/src/main/kotlin/nodes/ForkNode.kt
@@ -10,13 +10,17 @@ import com.group7.policies.fork.RandomForkPolicy
 /** Takes in a vehicle, and emits it in any one of its destination, as long as the output channel is open */
 class ForkNode<T>(
     label: String,
-    source: InputChannel<T>,
-    destinations: List<OutputChannel<T>>,
+    private val source: InputChannel<T>,
+    private val destinations: List<OutputChannel<T>>,
     private val policy: ForkPolicy<T> = RandomForkPolicy(),
 ) : Node(label, destinations) {
 
     init {
         source.onReceive { emit(it) }
+    }
+
+    context(_: Simulator)
+    override fun onStart() {
         policy.initialize(source, destinations)
     }
 
--- ports/src/main/kotlin/policies/fork/ForkPolicy.kt
+++ ports/src/main/kotlin/policies/fork/ForkPolicy.kt
@@ -2,6 +2,7 @@ package com.group7.policies.fork
 
 import com.group7.InputChannel
 import com.group7.OutputChannel
+import com.group7.Simulator
 
 interface ForkPolicy<T> {
     fun selectChannel(obj: T): OutputChannel<T>
@@ -12,10 +13,17 @@ interface ForkPolicy<T> {
 
     fun allClosed(): Boolean
 
+    context(_: Simulator)
     fun initialize(source: InputChannel<T>, destinations: List<OutputChannel<T>>) {
+        if (destinations.any { it.isOpen() }) {
+            source.open()
+        } else {
+            source.close()
+        }
         for (destination in destinations) {
-            require(destination.isOpen()) { "Channel $destination was closed prematurely" }
-            onChannelOpen(destination)
+            if (destination.isOpen()) {
+                onChannelOpen(destination)
+            }
 
             destination.whenOpened {
                 onChannelOpen(destination)
