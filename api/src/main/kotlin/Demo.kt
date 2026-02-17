0eb890dc69f794099c7c4dfd65f862eb16d8f9f9
ms6123 <ms6123@imperial.ac.uk>
2026-02-17 15:10:35 +0000

    Fix: Fix PR conflicts.

--- api/src/main/kotlin/Demo.kt
+++ api/src/main/kotlin/Demo.kt
@@ -3,14 +3,14 @@ package com.group7
 import com.group7.dsl.trackAll
 import com.group7.dsl.withMetrics
 import com.group7.metrics.Occupancy
-import com.group7.metrics.ServiceTime
+import com.group7.metrics.ResidenceTime
 import kotlin.time.Duration.Companion.days
 
 internal fun main() {
     val scenario =
         generatePort().first.withMetrics {
             trackAll(Occupancy)
-            trackAll(ServiceTime)
+            trackAll(ResidenceTime)
         }
     runSimulation(scenario, 10.days)
 }
