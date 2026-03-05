3bdbb2f0b97f47d4f3496d9a22cf296516665357
Squalm <43246103+Squalm@users.noreply.github.com>
2026-03-05 14:51:55 +0000

    Docs: means and steady state

--- ports/src/main/kotlin/metrics/SampleMean.kt
+++ ports/src/main/kotlin/metrics/SampleMean.kt
@@ -2,11 +2,17 @@ package com.group7.metrics
 
 import kotlin.time.Instant
 
+/**
+ * Calculate the sample mean of an instantaneous metric by dividing the sum of all values by the count.
+ *
+ * Is a continuous metric.
+ */
 class SampleMean(val raw: InstantaneousMetric) : ContinuousMetric() {
     private var count = 0
     private var sum = 0.0
 
     init {
+        // Count readings from instantaneous metrics
         raw.onFire { _, value ->
             count++
             sum += value
--- ports/src/main/kotlin/metrics/SteadyStateDetector.kt
+++ ports/src/main/kotlin/metrics/SteadyStateDetector.kt
@@ -2,6 +2,8 @@ package com.group7.metrics
 
 import kotlin.time.Instant
 
+/** Steady state detectors check if something is in a steady state so we can start calculating confidence intervals. */
 fun interface SteadyStateDetector {
+    /** Takes a time and returns `true` if the metric is steady. */
     fun isSteady(currentTime: Instant): Boolean
 }
