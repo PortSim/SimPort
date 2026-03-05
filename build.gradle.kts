aa3059daa5da4806b2516b4f863094c4d47de753
Aldiyar <aldiyar.yerzhanov23@imperial.ac.uk>
2026-03-05 19:17:27 +0000

    dokka setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins { id("convention") }
 
 dependencies { api(project(":simulator")) }
+
+dependencies {
+    dokka(project(":ports"))
+    dokka(project(":simulator"))
+    dokka(project(":visuals"))
+    dokka(project(":api"))
+    dokka(project(":demos"))
+}
--- buildSrc/src/main/kotlin/convention.gradle.kts
+++ buildSrc/src/main/kotlin/convention.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("plugin.power-assert")
     id("io.kotest")
     id("com.ncorti.ktfmt.gradle")
+    id("org.jetbrains.dokka")
     `maven-publish`
 }
 
@@ -37,6 +38,9 @@ dependencies {
     testImplementation("io.kotest:kotest-framework-engine:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+
+    // Dokka
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
 }
 
 kotlin {
@@ -56,6 +60,16 @@ powerAssert { functions = listOf("io.kotest.matchers.shouldBe") }
 ktfmt {
     kotlinLangStyle()
     maxWidth = 120
+}
+
+dokka {
+    dokkaSourceSets.configureEach {
+        skipDeprecated = true // Skips deprecated API
+        reportUndocumented = true // Reports if any public API are undocumented
+
+        jdkVersion = 21
+        moduleName = project.name
+    }
 }
 
 publishing { publications { create<MavenPublication>("maven") { from(components["java"]) } } }
