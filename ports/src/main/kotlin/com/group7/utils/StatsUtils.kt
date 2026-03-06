cb67372d4a0b36273557ac1995a130b8d98dcbda
pkl23 <pkl23@ic.ac.uk>
2026-03-06 15:36:35 +0000

    Docs: StatsUtils.kt

--- ports/src/main/kotlin/com/group7/utils/StatsUtils.kt
+++ ports/src/main/kotlin/com/group7/utils/StatsUtils.kt
@@ -4,8 +4,15 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 import umontreal.ssj.probdist.StudentDist
 
+/** Caches calculated student-t values previously encountered during execution */
 private val studentCache: ConcurrentMap<StudentKey, Double> = ConcurrentHashMap()
 
+/**
+ * Calculates the student-T value
+ *
+ * @param df degrees of freedom
+ * @param alpha alpha
+ */
 fun studentT(df: Int, alpha: Double): Double =
     studentCache.computeIfAbsent(StudentKey(df, alpha)) { StudentDist.inverseF(df, 1.0 - alpha / 2) }
 
